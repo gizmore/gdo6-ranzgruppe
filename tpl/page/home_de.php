5f2af13ab6c0a7acf1440f1733f59c622f9b11c8
gizmore <gizmore@wechall.net>
2020-10-28 15:36:01 +0100

    6.10-r9326

--- tpl/page/home_de.php
+++ tpl/page/home_de.php
@@ -1,13 +1,15 @@
+<div class="gdo-panel">
 <h1>Ranzgruppe!</h1>
 
-<p>Willkommen bei Ranzgruppe, der größten Punkband im bekannten Universum!</p>
+<p>Willkommen bei Ranzgruppe, der bald größten Punkband im bekannten Universum!</p>
 
-<h3>Was ist Ranzgruppe?</h3>
+<h2>Was ist Ranzgruppe?</h2>
 
 <p>Ranzgruppe ist eine Punk-Band aus Peine, Niedersachsen. Jeder kann mitmachen solange er ein Instrument spielen kann.</p>
 <p>Die Band tritt niemals komplett auf, wir sind ja kein Orchester. Dafür können mehrere Gigs synchron stattfinden.</p>
 
-<h3>Wer ist Ranzgruppe?</h3>
+<h2>Wer ist Ranzgruppe?</h2>
 
 <p>Ranzgruppe wurde im August 2019 inoffiziell gegründet.</p>
 <p>Die Gründer, gizmore(vocals) und lazer(Schädelklatscher) haben einige Songs &quot;komponiert&quot;, hatten allerdings noch nie einen Auftritt.</p>
+</div>
--- tpl/page/home_en.php
+++ tpl/page/home_en.php
@@ -1,14 +1,13 @@
+<div class="gdo-panel">
 <h1>Ranzgruppe!</h1>
+<p>Welcome to Ranzgruppe, the soon biggest Punkband in the known Universe!</p>
 
-<p>Welcome to Ranzgruppe, the biggest Punkband in the known Universe!</p>
-
-<h3>What ist Ranzgruppe?</h3>
-
+<h2>What ist Ranzgruppe?</h2>
 <p>Ranzgruppe is a punk band from Peine, Lower saxony.
 Everyone can participate, as long as they know how to sing or play an instrument.</p>
 <p>The band never performs with all members together, we are not an orchestra. As compensation we can have multiple gigs simultaniously.</p>
 
-<h3>Who is Ranzgruppe?</h3>
-
+<h2>Who is Ranzgruppe?</h2>
 <p>Ranzgruppe has been founded inofficially in August'2019.</p>
 <p>The founders, gizmore(vocals) and lazer(skullclapper) have &quot;composed&quot; a few songs, but never had a gig.</p>
+</div>
