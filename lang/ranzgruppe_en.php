92203900865fa6d9c4c03c98f1e2fd793deff86a
gizmore <gizmore@wechall.net>
2021-01-05 18:26:52 +0100

    6.10-r9431_sync

--- lang/ranzgruppe_en.php
+++ lang/ranzgruppe_en.php
@@ -5,4 +5,7 @@ return array(
 'link_join' => 'Join Ranzgruppe!',
 'link_gigs' => 'Events',
 'link_albums' => 'Albums',
+
+'mtitle_ranzgruppe_home' => 'Ranzgruppe Website!',
+'mdescr_ranzgruppe_home' => 'Ranzgruppe wird die größte Punkband des Universums.',
 );
